e9255bca6d4a76d64614c0be850638ad2b838ce9
Umar Naseer <156530819+umar057@users.noreply.github.com>
2024-01-15 01:54:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,10 @@ async def predict(request: PredictionRequest):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
+@app.get("/")
+def index():
+    return {"details":"Hello!!"}
+    
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='127.0.0.1', port=8000)
